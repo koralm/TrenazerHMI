fc1e5bd1c4c85033c17cb9f2a7976b400295e631
koralm <koralm.for@gmail.com>
2015-09-08 01:38:39 +0200

    Komunikacja OK, Sprawidzić -Przetestować zapis do pliku -Obczaic wykresy

--- server_js/rs232.js
+++ server_js/rs232.js
@@ -1,63 +1,107 @@
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
 
-var sp_ov_USB = new SerialPort("COM6", {
-    baudrate: 1000000,
-    buffersize: 4096,
-    parser: serialport.parsers.readline("03037e7e",'hex')
+/*
+* INITIALIZE VARIABLES AND COM
+ */
+//Port settings
+var COM_port = "COM4"
+var COM_baudrate = 1000000;
+var COM_buffer_size = 4096;
+var COM_parse_strig = "03037e7e";
+
+//Open Serial port
+var sp_ov_USB = new SerialPort(COM_port, {
+    baudrate: COM_baudrate,
+    buffersize: COM_buffer_size,
+    parser: serialport.parsers.readline(COM_parse_strig,'hex')
 }, false);
 
-var sample = 0;
-var rs_status=4;
-    rs_status.toString(16);
-var rs_llength = 0;
-    rs_llength.toString(16);
-var rs_minteria = 00;
-    rs_minteria.toString(16);
+/*
+ * INITIALIZE VARIABLES FOR COMMUNICATION BETWEEN MODULES
+ */
+//Recieved from COM
+var decoded_data = 0;
 
+//SEND over COM
+var time_interval = 1000;        //Send data interval
+var frame_header = '7E7E';      //Frame header
+var rs_status = 0;              //For calibration status 0,4,1
+var rs_line_length = 0;         //Line length
+var rs_interia = 0;             //Interia moment x*0.001 eg: 1000*0.001=1
+var frame_terminator = '0303';  //Frame terminator
 
 sp_ov_USB.open(function (error) {
     if ( error ) {
-        console.log('failed to open: '+error);
+        console.log('failed to open COM: '+error);
     } else {
-        console.log('open');
+        console.log('COM open');
 
        //RECEIVE DATA
         sp_ov_USB.on('data', function(data) {
-            console.log('data received: ' + data);
-            var bufferek = new Buffer(data ,'hex');
-            var sample_nr = bufferek.readUInt16LE(17);
-            var strength = bufferek.readDoubleLE(0);
-            var position = bufferek.readDoubleLE(8);
-            var induction_sens = bufferek.readUInt8(16);
-            sample = position;
 
-            console.log('probka: ' + sample_nr.toString());
-            console.log('sila: ' + strength.toString());
-            console.log('polozenie: ' + position.toString());
-            console.log('czujnik: ' + induction_sens.toString());
+            //decode data
+            decoded_data = decode_recev_data(data);
+
+            //CONSOLE display received data
+            //disp_recev_data(decoded_data);
         });
 
-        //SENDDATA
-       // exports.rs_stat = function (data) {
-
-
-            var rsout_head = new Buffer('7E7E' ,'hex');
-            var rsout_exit = new Buffer('0303' ,'hex');
-            var send_buffer1 = new Buffer(4);
-            send_buffer1.fill(0);
-            send_buffer1.writeUInt8(rs_status,0);
-            send_buffer1.writeUInt8(rs_llength,1);
-            send_buffer1.writeUInt16BE(rs_minteria,2);
-            var rs_frameout = Buffer.concat([rsout_head,send_buffer1,rsout_exit]);
+        //SEND DATA
             setInterval( function() {
-                sp_ov_USB.write(rs_frameout);
-            }, 1 );
+                // SendFrame
+                var send_frame = [frame_header,rs_status,rs_line_length,rs_interia,frame_terminator]; //Full Frame
+
+                //Push_data
+                sp_ov_USB.write(code_send_data(send_frame));
+
+                //CONSOLE display send data
+                console.log(code_send_data(send_frame));
+            }, time_interval );
         //};
 
     }
 });
 
-exports.rsout = function () {
-    return sample;
+function decode_recev_data(data){
+    var bufferek = new Buffer(data ,'hex');
+    var sample_nr = bufferek.readUInt16LE(17);
+    var strength = bufferek.readDoubleLE(0);
+    var position = bufferek.readDoubleLE(8);
+    var induction_sens = bufferek.readUInt8(16);
+    return([data,sample_nr,strength,position,induction_sens]);
+}
+
+function disp_recev_data(data){
+    console.log('data received: ' + data[0]);
+    console.log('probka: ' + data[1].toString());
+    console.log('sila: ' + data[2].toString());
+    console.log('polozenie: ' + data[3].toString());
+    console.log('czujnik: ' + data[4].toString());
+}
+
+function code_send_data(send_frame){
+    var header = new Buffer(send_frame[0] ,'hex');
+    var send_buffer = new Buffer(4);
+        send_buffer.writeUInt8('0x' + send_frame[1].toString(16),0,'hex');
+        send_buffer.writeUInt8('0x' + send_frame[2].toString(16),1,'hex');
+        send_buffer.writeUInt16LE('0x' + send_frame[3].toString(16),2,'hex');
+    var terminator = new Buffer(send_frame[4] ,'hex');
+    var rs_frameout = Buffer.concat([header,send_buffer,terminator]);
+    return(rs_frameout);
+}
+
+
+exports.rs_status = function (data) {
+    rs_status = data;
 };
+
+exports.rs_line_length = function (data) {
+    rs_line_length = data;
+};
+
+exports.rs_interia = function (data) {
+    rs_interia = data;
+};
+
+
