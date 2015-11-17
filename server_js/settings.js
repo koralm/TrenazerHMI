2cc351ceb0ca577408c870cca958c948852fa568
koralm <koralm.for@gmail.com>
2015-11-17 16:57:13 +0100

    Graf przetestowany i naprawiony Pocztek przeróbek zapisu ustawien do pliku
    Zapis ustwaien do pliku dziala
    Przetestowac wprowaadzone ustawienia
    Dodac zapytanie o ustawieia podczas cwiczenia
    
    Przekazywanie wartości:
    -stoper
    -kalibracja
    -dodać 2 wysiwetlacz
    -asynchroniczne granie

--- server_js/settings.js
+++ server_js/settings.js
@@ -1,10 +1,15 @@
 var fs = require('fs');
 
-var settings_default = "";
+var settings_default = JSON.parse(fs.readFileSync("./settings/default.init", 'utf8'));
+var settings_loaded = ""
+var settings_actual = settings_default;
+
 
 exports.save = function (data){
+    settings_loaded = data;
     wstream_c = fs.createWriteStream("./settings/" + data.settings_name.toString() + ".init");
     wstream_c.write(JSON.stringify(data, null, 2));
+    settings_actual = settings_loaded;
 }
 
 exports.load_def = function (){
@@ -12,3 +17,4 @@ exports.load_def = function (){
     return (settings_default);
 }
 
+exports.settings_actual = settings_actual;
