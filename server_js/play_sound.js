ba6d7da421efd53f6566410684845101811b01a9
koralm <koralm.for@gmail.com>
2016-05-25 15:16:15 +0200

    zmainy w funkcji play, zamiast odczytu z dysku zaladowanie do zmiennych

--- server_js/play_sound.js
+++ server_js/play_sound.js
@@ -2,11 +2,32 @@ var fs = require('fs');
 var lame = require('lame');
 var Speaker = require('speaker');
 
+var range_up1 = fs.createReadStream('./sounds/' + 'range_up1' +'.mp3');
+var range_down1 = fs.createReadStream('./sounds/' + 'range_down1' +'.mp3');
+
+var range_up2 = fs.createReadStream('./sounds/' + 'range_up2' +'.mp3');
+var range_down2 = fs.createReadStream('./sounds/' + 'range_down2' +'.mp3');
+var TaDa = fs.createReadStream('./sounds/' + 'TaDa' +'.mp3');
 
 exports.play_end = function(data) {
-    fs.createReadStream('./sounds/' + data +'.mp3')
+    if (data==range_up1) {
+        range_up1
+            .pipe(new lame.Decoder)
+            .pipe(new Speaker);
+    } else if (data==range_up2) {
+        range_up2
+            .pipe(new lame.Decoder)
+            .pipe(new Speaker);
+    } else if (data==range_down1) {
+        range_down1
+                .pipe(new lame.Decoder)
+                .pipe(new Speaker);
+    } else if (data==range_down2) {
+        range_down2
+            .pipe(new lame.Decoder)
+            .pipe(new Speaker);
+    } else if (data==TaDa) {
+    range_down2
         .pipe(new lame.Decoder)
-        //.on('format', console.log)
-        .pipe(new Speaker);
-}
+        .pipe(new Speaker);}}
 
