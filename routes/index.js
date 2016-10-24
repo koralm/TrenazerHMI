adc581df34cc9880639adec163161bd32395c0d8
koralm <koralm.for@gmail.com>
2016-10-24 16:32:30 +0200

    Dodane przejście odrazu do ćwiczenia

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' } );
 });
 
 router.get('/exercise', function(req, res, next) {
-  res.render('exercise', { title: 'Exercise' });
+  res.render('exercise', {title: 'Exercise', load_where: 0});
+});
+
+router.get("/exercise_direct", function(req, res, next) {
+  res.render('exercise', {title: 'Exercise', load_where: 1});
 });
 
 router.get('/settings', function(req, res, next) {
